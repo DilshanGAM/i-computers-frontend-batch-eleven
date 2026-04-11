7721e9a1bad0f23bbc007c89cd16009c94ca24e5
DilshanGAM <malithdilshan27@gmail.com>
2026-04-11 20:58:52 +0530

    Day 12 ✏️

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import HomePage from './pages/homePage'
 import LoginPage from './pages/loginPage'
 import RegisterPage from './pages/registerPage'
 import AdminPage from './pages/adminPage'
+import TestPage from './pages/test'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   
   
   return (
     <div className='w-full h-screen '>
+      <Toaster position='top-right'/>
       <Routes>
 
         <Route path='/'  element={<HomePage/>}  />
@@ -19,6 +22,8 @@ function App() {
         <Route path='/signup' element={<RegisterPage/>}/>
 
         <Route path='/admin/*' element={<AdminPage/>}/>
+
+        <Route path='/test' element={<TestPage/>}/>
 
       </Routes>
     </div>
---
+++ src/pages/test.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react"
+import toast from "react-hot-toast"
+import { FaTwitter } from "react-icons/fa6";
+
+export default function TestPage(){
+
+    const [score , setScore] =  useState(50)
+    const [mood , setMood] = useState("😐")
+    const [isFollowed , setIsFollowed] = useState(false)
+
+    //let score = 50
+
+    
+
+    return(
+        <div className="w-full h-full bg-green-400 flex justify-center items-center">
+            <div className="w-[450px] h-[450px]  bg-white flex justify-center items-center flex-col">
+                <h1 className="font-bold text-7xl">{score}</h1>
+                <div className="w-full h-[100px] flex justify-center items-center">
+                    <button className="w-[100px] bg-red-600 h-[40px] mx-5"
+                        onClick={
+                            ()=>{
+                                // score = 49
+                                setScore(score - 1)
+                            }
+                        }>
+                        Decrease
+                    </button>
+                    <button 
+                        className="w-[100px] bg-green-600 h-[40px] mx-5"
+                        onClick={
+                            ()=>{
+
+                                setScore(score + 1)
+
+                            }
+                        }>
+                        Increase
+                    </button>
+                </div>
+                <h1 className="font-bold text-7xl">{mood}</h1>
+                 <div className="w-full h-[100px] flex justify-center items-center">
+                    <button className="w-[100px] bg-red-600 h-[40px] mx-5"
+                        onClick={
+                            ()=>{
+                                setMood("☹️")
+                                toast.error("Oh no! You are sad")
+                            }
+                        }>
+                        Sad
+                    </button>
+                    <button 
+                        className="w-[100px] bg-green-600 h-[40px] mx-5"
+                        onClick={
+                            ()=>{
+                                setMood("😐")
+                                toast("You are neutral",{
+                                    icon: "😐"
+                                })
+                            }
+                        }>
+                        Neurtal
+                    </button>
+                     <button 
+                        className="w-[100px] bg-blue-600 h-[40px] mx-5"
+                        onClick={
+                            ()=>{
+                                toast.success("Yay! You are happy")
+                                setMood("😀")
+                            }
+                        }>
+                        Happy
+                    </button>
+                        
+                </div>
+                <FaTwitter onClick={
+                    ()=>{
+                        toast("Follow us on Twitter",{
+                            icon: <FaTwitter className="text-blue-500"/>
+                        })
+                        setIsFollowed(!isFollowed)
+                    }
+                } 
+                className={isFollowed ? "text-[100px] text-blue-600" : "text-[100px] text-gray-600"}/>                
+            </div>
+        </div>
+    )
+}
